9d1b1b17eeca1ab8d22a7c719bf2aef76b7c77e5
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2017-03-03 16:47:37 -0800

    install blasfeo and hpmpc to usr/lib

--- cmake/external/hpmpc.cmake
+++ cmake/external/hpmpc.cmake
@@ -17,7 +17,7 @@ ExternalProject_Add(
     SOURCE_DIR "${PROJECT_SOURCE_DIR}/external/hpmpc"
     BUILD_IN_SOURCE 1
     BUILD_COMMAND make clean static_library -j 2 TARGET=${HPMPC_TARGET} BLASFEO_PATH=${HPMPC_BLASFEO_PATH}
-    INSTALL_COMMAND ""
+		INSTALL_COMMAND cp libhpmpc.a /usr/lib/libhpmpc.a
 )
 
 ExternalProject_Get_Property(hpmpc_project source_dir)
