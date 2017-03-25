fe87245f082c00407bc8e9a77ea9a4c5eca78808
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2017-03-24 17:12:26 -0700

    separate build and install

--- cmake/external/blasfeo.cmake
+++ cmake/external/blasfeo.cmake
@@ -13,20 +13,20 @@ endif()
 
 ExternalProject_Add(
     blasfeo_project
-
     CONFIGURE_COMMAND ""
     SOURCE_DIR "${PROJECT_SOURCE_DIR}/external/blasfeo"
     BUILD_IN_SOURCE 1
     BUILD_COMMAND make clean shared_library -j 2 TARGET=${BLASFEO_TARGET} LA=${BLASFEO_LA}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-		INSTALL_COMMAND make install PREFIX=${CMAKE_INSTALL_PREFIX}
-  #  INSTALL_COMMAND ""
-
+    STEP_TARGETS build install
+    EXCLUDE_FROM_ALL TRUE
+	#	INSTALL_COMMAND make install PREFIX=${CMAKE_INSTALL_PREFIX}
+  # INSTALL_COMMAND ""
 )
 
 set_target_properties(blasfeo_project PROPERTIES VERSION 0.22)
 set_target_properties(blasfeo_project PROPERTIES SOVERSION 0.22)
 ExternalProject_Get_Property(blasfeo_project source_dir)
 add_library(blasfeo SHARED IMPORTED)
-add_dependencies(blasfeo blasfeo_project)
+#add_dependencies(blasfeo blasfeo_project-build)
 set_property(TARGET blasfeo PROPERTY IMPORTED_LOCATION "${source_dir}/libblasfeo.so")
--- cmake/external/hpmpc.cmake
+++ cmake/external/hpmpc.cmake
@@ -17,11 +17,13 @@ ExternalProject_Add(
     BUILD_IN_SOURCE 1
     BUILD_COMMAND make clean shared_library -j 2 TARGET=${HPMPC_TARGET} BLASFEO_PATH=${HPMPC_BLASFEO_PATH}
 		CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+		STEP_TARGETS build install
+		EXCLUDE_FROM_ALL TRUE
 #		INSTALL_COMMAND make install PREFIX=${CMAKE_INSTALL_PREFIX}
 #		INSTALL_COMMAND ""
 )
 
 ExternalProject_Get_Property(hpmpc_project source_dir)
 add_library(hpmpc SHARED IMPORTED)
-add_dependencies(hpmpc hpmpc_project blasfeo_project)
-set_property(TARGET hpmpc PROPERTY IMPORTED_LOCATION "${source_dir}/libhpmpc.so")
+#add_dependencies(hpmpc hpmpc_project-build blasfeo_project-build)
+#set_property(TARGET hpmpc PROPERTY IMPORTED_LOCATION "${source_dir}/libhpmpc.so")
